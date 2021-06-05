cb1dd8d85332865db407b78a948082f5d6f807c7
Johnr302 <johnathon.regalado@gmail.com>
2021-06-04 17:13:00 -0700

    function to convert a number to array

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,23 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import Button from "react-bootstrap/Button";
 
 export default function Form() {
   const [number, setNumber] = useState(null);
   const [inputNumber, setInputNumber] = useState(null);
+  let numberArray = [];
+  const numToArray = (num) => {
+    numberArray = String(num)
+      .split("")
+      .map((num) => {
+        return Number(num);
+      });
+    console.log(numberArray);
+  };
+
+  useEffect(() => {
+    numToArray(number);
+    console.log("useE", numberArray);
+  }, [number]);
 
   return (
     <div>
