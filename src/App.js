700b23c7210a686a954c9b433da95003bc3eca83
Johnr302 <johnathon.regalado@gmail.com>
2021-06-08 20:15:44 -0700

    moved logic from Form component to App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import "./App.css";
+import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Form from "./components/Form";
 
 function App() {
+  const [number, setNumber] = useState(null);
+  let numberArray = [];
+  const numToArray = (num) => {
+    numberArray = String(num)
+      .split("")
+      .map((num) => {
+        return Number(num);
+      });
+    console.log(numberArray);
+  };
+
+  useEffect(() => {
+    numToArray(number);
+  }, [number]);
+
   return (
     <div className="App">
       <h1>Divisibility App</h1>
-      <Form />
+      <Form setNumber={setNumber} />
     </div>
   );
 }
